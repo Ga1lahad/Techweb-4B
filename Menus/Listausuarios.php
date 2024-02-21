99680bb225fb5139c0adbb83c996c72349c9f7f1
Ga1lahad <151469046+Ga1lahad@users.noreply.github.com>
2024-02-21 19:15:33 -0300

    Inicio do Main

---
+++ Menus/Listausuarios.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuarios</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <nav>
+    <a href="../"><img class="re" src="../utilidades/imgs/re.png" alt="Seta de Retorno"></a>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Inicial</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav></nav>
+    <div class="centralizado">
+        <h1>Selecione das opções abaixo<br>qual menu deseja acessar:</h1>
+    </div>
+    <div class="menus">
+        <a class="aindex" href="Menus/Cadastro.php">Cadastros</a>
+        <a class="aindex" href="Menus/Listausuarios.php">Listar usuarios</a>
+
+    </div>
+</body>
+
+</html>
